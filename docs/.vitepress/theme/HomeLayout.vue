4a251fc507aba75a232d2e9993a62571f951260f
LogneBudo <33702828+LogneBudo@users.noreply.github.com>
2026-03-16 07:26:29 +0100

    Replace neural network background with lakeside landscape (day/night for light/dark mode)

--- docs/.vitepress/theme/HomeLayout.vue
+++ docs/.vitepress/theme/HomeLayout.vue
@@ -1,12 +1,30 @@
 <script setup lang="ts">
 import DefaultTheme from 'vitepress/theme'
 import { useData } from 'vitepress'
-import NeuralBackground from './NeuralBackground.vue'
 
 const { frontmatter } = useData()
 </script>
 
 <template>
-  <NeuralBackground v-if="frontmatter.layout === 'home'" />
+  <div v-if="frontmatter.layout === 'home'" class="landscape-bg" />
   <DefaultTheme.Layout />
 </template>
+
+<style scoped>
+.landscape-bg {
+  position: fixed;
+  inset: 0;
+  z-index: -1;
+  pointer-events: none;
+  background-image: url('/lakeside-day.png');
+  background-size: cover;
+  background-position: center bottom;
+  background-repeat: no-repeat;
+  opacity: 0.25;
+}
+
+:global(.dark) .landscape-bg {
+  background-image: url('/lakeside-night.png');
+  opacity: 0.35;
+}
+</style>
